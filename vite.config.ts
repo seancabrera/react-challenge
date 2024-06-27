34f87682d7cdacd323b15b040c0c0d6faca025fd
Sean Cabrera <sean.cabrera@gmail.com>
2024-06-27 10:27:58 -0600

    Make separate vitest.config.ts for tests
    Ran into linting errors importing both viteTsConfigPaths and tsConfigPaths
    in vite.config.ts, but I can only get tests to work with tsConfigPaths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), tsconfigPaths(), svgrPlugin()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   optimizeDeps: {
     force: true,
     esbuildOptions: {
@@ -14,9 +13,5 @@ export default defineConfig({
         '.js': 'jsx'
       }
     }
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  },
+  }
 });
